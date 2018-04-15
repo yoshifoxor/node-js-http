2ce902f3a2335dc32c035a6146d9f17c422cec0b
akanekwon <kurmelsa5d@gmail.com>
2018-04-15 19:02:44 +0000

    Basic認証とlogout実装

--- index.js
+++ index.js
@@ -2,10 +2,27 @@
 
 const http = require('http');
 const jade = require('jade');
-const server = http.createServer((req, res) => {
+const auth = require('http-auth');
+const basic = auth.basic({
+    realm: 'Enter username and password.'
+  },
+  (username, password, callback) => {
+    callback(username === 'guest' && password === 'xaXZJQmE');
+  });
+
+//const server = http.createServer((req, res) => {
+const server = http.createServer(basic, (req, res) => {
     //console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
-    const now = new Date();
-    console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+    //const now = new Date();
+    console.info('Requested by ' + req.connection.remoteAddress);
+
+    if (req.url === '/logout') {
+      res.writeHead(401, {
+        'Content-Type': 'text/plain; charset=utf-8'
+      });
+      res.end('ログアウトしました');
+      return;
+    }
 
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8'
@@ -18,18 +35,19 @@ const server = http.createServer((req, res) => {
         //const fs = require('fs');
         //const rs = fs.createReadStream('./form.html');
         //rs.pipe(res);
-        if (req.url === '/enquetes/yaki-shabu') {
-          res.write(jade.renderFile('./form.jade', {
-            path: req.url,
-            firstItem: '焼き肉',
-            secondItem: 'しゃぶしゃぶ'
-          }));
-        } else if (req.url === '/enquetes/rice-bread') {
-          res.write(jade.renderFile('./form.jade', {
-            path: req.url,
-            firstItem: 'ごはん',
-            secondItem: 'パン'
-          }));
+        let urls = ['/yaki-shabu', '/rice-bread', '/sushi-pizza'];
+        let firstItems = ['焼き肉', 'ごはん', '寿司'];
+        let secondItems = ['しゃぶしゃぶ', 'パン', 'ピザ'];
+
+        for (let i = 0; i < urls.length; i++) {
+          if (req.url === '/enquetes' + urls[i]) {
+            res.write(jade.renderFile('./form.jade', {
+              path: req.url,
+              firstItem: firstItems[i],
+              secondItem: secondItems[i]
+            }));
+            break;
+          }
         }
         res.end();
         break;
@@ -43,7 +61,7 @@ const server = http.createServer((req, res) => {
           body = Buffer.concat(body).toString();
           //console.info('[' + now + '] Data posted: ' + body);
           const decoded = decodeURIComponent(body);
-          console.info('[' + now + '] 投稿: ' + decoded);
+          console.info('投稿: ' + decoded);
           res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
             decoded + 'が投稿されました</h1></body></html>');
           res.end();
@@ -56,14 +74,14 @@ const server = http.createServer((req, res) => {
     //res.end();
   })
   .on('error', (e) => {
-    console.error('[' + new Date() + '] Server Error', e);
+    console.error('Server Error', e);
   }).on('clientError', (e) => {
-    console.error('[' + new Date() + '] Client Error', e);
+    console.error('Client Error', e);
   });
 
 //const port = 8000;
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
   //console.log('Listening on ' + port);
-  console.info('[' + new Date() + ']Listening on ' + port);
+  console.info('Listening on ' + port);
 });
