8631897db73d7fe9ad482829d659d7047658cdb3
yoshifoxor <kurmelsa5d@gmail.com>
2019-09-17 06:44:34 +0900

    HTML のフォーム

--- index.js
+++ index.js
@@ -5,26 +5,29 @@ const server = http.createServer((req, res) => {
   const now = new Date();
   console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
   switch (req.method) {
     case 'GET':
-      res.write('GET ' + req.url);
+      const fs = require('fs');
+      const rs = fs.createReadStream('./form.html');
+      rs.pipe(res);
       break;
     case 'POST':
-      res.write('POST ' + req.url);
       let body = [];
       req.on('data', (chunk) => {
         body.push(chunk);
       }).on('end', () => {
         body = Buffer.concat(body).toString();
-        console.info('[' + now + '] Data posted: ' + body);
+        const decoded = decodeURIComponent(body);
+        console.info('[' + now + '] 投稿: ' + decoded);
+        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' + decoded + 'が投稿されました</h1></body></html>');
+        res.end();
       });
       break;
     default:
       break;
   }
-  res.end();
 }).on('error', (e) => {
   console.error('[' + new Date() + '] Server Error', e);
 }).on('clientError', (e) => {
