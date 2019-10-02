42dbee02f877fb166d1d3200580bb9d2005c3952
yoshifoxor <kurmelsa5d@gmail.com>
2019-10-03 00:49:52 +0900

    HTTP サーバーの作成

--- index.js
+++ index.js
@@ -1,56 +1,13 @@
 'use strict';
 const http = require('http');
-const pug = require('pug');
 const server = http.createServer((req, res) => {
-  const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8'
+    'Content-Type': 'text/plain; charset=utf-8'
   });
-  switch (req.method) {
-    case 'GET':
-      if(req.url === '/enquetes/yaki-shabu') {
-        res.write(pug.renderFile('./form.pug', {
-          path: req.url,
-          firstItem: '焼肉',
-          secondItem: 'しゃぶしゃぶ'
-        }));
-      } else if (req.url === '/enquetes/rice-bread') {
-        res.write(pug.renderFile('./form.pug', {
-          path: req.url,
-          firstItem: 'ごはん',
-          secondItem: 'パン'
-        }));
-      } else if (req.url === '/enquetes/sushi-pizza') {
-        res.write(pug.renderFile('./form.pug', {
-          path: req.url,
-          firstItem: '寿司',
-          secondItem: 'ピザ'
-        }));
-      }
-      res.end();
-      break;
-    case 'POST':
-      let body = [];
-      req.on('data', (chunk) => {
-        body.push(chunk);
-      }).on('end', () => {
-        body = Buffer.concat(body).toString();
-        const decoded = decodeURIComponent(body);
-        console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' + decoded + 'が投稿されました</h1></body></html>');
-        res.end();
-      });
-      break;
-    default:
-      break;
-  }
-}).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
-}).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
+  res.write(req.headers['user-agent']);
+  res.end();
 });
-const port = process.env.PORT || 8000;
+const port = 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.log('Listening on ' + port);
 });
