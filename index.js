dd937c353032fb9f6ef38cb34ba4faba9842b117
yoshifoxor <yshs.adachi@gmail.com>
2021-05-02 22:28:41 +0900

    認証で利用者を制限する

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 'use strict';
 const http = require('http');
 const pug = require('pug');
-const server = http.createServer((req, res) => {
-  const now = new Date();
-  console.info(`[${now}] Requested by ${req.connection.remoteAddress}`);
+const auth = require('http-auth');
+const basic = auth.basic({
+  realm: 'Enquetes Area.',
+  file: __dirname + '/users.htpasswd',
+});
+const server = http.createServer(basic.check((req, res) => {
+  console.info(`Requested by ${req.connection.remoteAddress}`);
+
+  if (req.url === '/logout') {
+    res.writeHead(401, {
+      'Content-Type': 'text/plain; charset=utf-8',
+    });
+    res.end('ログアウトしました');
+    return;
+  }
+
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8',
   });
@@ -11,17 +24,9 @@ const server = http.createServer((req, res) => {
   switch (req.method) {
     case 'GET':
       if (req.url === '/') {
-        res.write('<!DOCTYPE html><html lang="ja"><body>' +
-          '<h1>アンケートフォーム</h1>' +
-          '<a href="/enquetes">アンケート一覧</a>' +
-          '</body></html>');
+        res.write(pug.renderFile('./index.pug'));
       } else if (req.url === '/enquetes') {
-        res.write('<!DOCTYPE html><html lang="ja"><body>' +
-          '<h1>アンケート一覧</h1><ul>' +
-          '<li><a href="/enquetes/yaki-shabu">焼き肉・しゃぶしゃぶ</a></li>' +
-          '<li><a href="/enquetes/rice-bread">ごはん・パン</a></li>' +
-          '<li><a href="/enquetes/sushi-pizza">寿司・ピザ</a></li>' +
-          '</ul></body></html>');
+        res.write(pug.renderFile('./enquetes.pug'));
       } else if (req.url === '/enquetes/yaki-shabu') {
         res.write(pug.renderFile('./form.pug', {
           path: req.url,
@@ -38,7 +43,7 @@ const server = http.createServer((req, res) => {
         res.write(pug.renderFile('./form.pug', {
           path: req.url,
           firstItem: '寿司',
-          secondItem: 'ピザ'
+          secondItem: 'ピザ',
         }));
       }
       res.end();
@@ -51,7 +56,7 @@ const server = http.createServer((req, res) => {
         const qs = require('querystring');
         const answer = qs.parse(rawData);
         const body = `${answer['name']}さんは${answer['favorite']}に投票しました`;
-        console.info(`[${now}] 投稿: ${body}`);
+        console.info(`投稿: ${body}`);
         res.write(`<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`);
         res.end();
       });
@@ -59,12 +64,12 @@ const server = http.createServer((req, res) => {
     default:
       break;
   }
-}).on('error', e => {
-  console.error(`[${new Date()}] Server Error`, e);
+})).on('error', e => {
+  console.error(`Server Error`, e);
 }).on('clientError', e => {
-  console.error(`[${new Date()}] Client Error`, e);
+  console.error(`Client Error`, e);
 });
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info(`[${new Date()}] Listening on ${port}`);
+  console.info(`Listening on ${port}`);
 });
