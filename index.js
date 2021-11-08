8aa55d7385455bb29239f9fb2e59610af9ce33a7
yoshifoxor <yshs.adachi@gmail.com>
2021-11-08 23:15:21 +0900

    HTML のフォーム

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const now = new Date();
     console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
     res.writeHead(200, {
-      'Content-Type': 'text/plain; charset=utf-8',
+      'Content-Type': 'text/html; charset=utf-8',
     });
 
     switch (req.method) {
       case 'GET':
-        res.write(`GET ${req.url}`);
+        const rs = fs.createReadStream('./form.html');
+        rs.pipe(res);
         break;
       case 'POST':
-        res.write(`POST ${req.url}`);
         let rawData = '';
 
         req.on('data', chunk => {
           rawData = rawData + chunk;
         }).on('end', () => {
-          console.info(`[${now}] Data posted: ${rawData}`);
+          const answer = new URLSearchParams(rawData);
+          const body = `${answer.get('name')}さんは${answer.get('yaki-shabu')}に投票しました`;
+          const html = `<!DOCTYPE html>
+            <html lang="ja">
+            <body><h1>${body}</h1></body>
+            </html>`;
+          console.info(`[${now}] 投稿: ${body}`);
+          res.write(html);
+          res.end();
         });
-        break;
-      case 'DELETE':
-        res.write(`DELETE ${req.url}`);
         break;
       default:
         break;
     }
-    res.end();
   }).on('error', e => {
     console.error(`[${new Date()}] Server Error`, e);
   }).on('clientError', e => {
