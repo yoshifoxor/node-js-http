9740ad8f260c3b28beba34829fff13b161262914
yoshifoxor <yshs.adachi@gmail.com>
2021-11-23 00:50:58 +0900

    コンソール出力の修正

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ const write = (req, res, ...data) => {
 };
 
 const server = http.createServer((req, res) => {
-    const now = new Date();
-    console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
+    console.info(`Requested by ${req.socket.remoteAddress}`);
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8',
     });
@@ -57,7 +56,7 @@ const server = http.createServer((req, res) => {
             <html lang="ja">
             <body><h1>${body}</h1></body>
             </html>`;
-          console.info(`[${now}] ${body}`);
+          console.info(`${body}`);
           res.write(html);
           res.end();
         });
@@ -66,12 +65,12 @@ const server = http.createServer((req, res) => {
         break;
     }
   }).on('error', e => {
-    console.error(`[${new Date()}] Server Error`, e);
+    console.error(`Server Error`, e);
   }).on('clientError', e => {
-    console.error(`[${new Date()}] Client Error`, e);
+    console.error(`Client Error`, e);
   });
 const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
-  console.info(`[${new Date()}] Listening on port:${port}`);
+  console.info(`Listening on port:${port}`);
 });
